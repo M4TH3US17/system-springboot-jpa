598f76107f3eacee570bd8edbe82b330b08fbf62
M4TH3US17 <matheusdalvino50@gmail.com>
2021-09-09 15:10:07 -0400

    H2 database, test profile, JPA

--- src/main/java/com/matheus/course/entities/User.java
+++ src/main/java/com/matheus/course/entities/User.java
@@ -3,9 +3,17 @@ package com.matheus.course.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User implements Serializable {
 	static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
