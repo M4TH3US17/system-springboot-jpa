d21e1caf97b3cd20e50617b512a9d22beb28e68f
M4TH3US17 <matheusdalvino50@gmail.com>
2021-09-27 17:08:21 -0400

    Payment, one-to-one association

--- src/main/java/com/matheus/course/config/TestConfig.java
+++ src/main/java/com/matheus/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.matheus.course.entities.Category;
 import com.matheus.course.entities.Order;
 import com.matheus.course.entities.OrderItem;
+import com.matheus.course.entities.Payment;
 import com.matheus.course.entities.Product;
 import com.matheus.course.entities.User;
 import com.matheus.course.entities.enums.OrderStatus;
@@ -75,6 +76,10 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 	}
 }
--- src/main/java/com/matheus/course/entities/Order.java
+++ src/main/java/com/matheus/course/entities/Order.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -13,6 +14,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -35,6 +37,9 @@ public class Order implements Serializable {
 	private User client;
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
+	
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
 	
 	public Order() {
 	}
@@ -63,7 +68,15 @@ public class Order implements Serializable {
 		this.moment = moment;
 	}
 	
-	public Set<OrderItem> getOrderItem(){
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
+	public Set<OrderItem> getItems(){
 		return items;
 	}
 
